0eb4e77ad4b679ac8ad9ec6e4aa07d8d866f1d37
agmota11 <a.gmota@alumnos.upm.es>
2023-05-12 18:55:01 +0200

    Improved Uformer TV

--- src/pytorch_Uformer/losses.py
+++ src/pytorch_Uformer/losses.py
@@ -5,57 +5,6 @@ import numpy as np
 from torch.autograd import Variable
 
 
-
-def tv_loss(x, beta = 0.5, reg_coeff = 5):
-    '''Calculates TV loss for an image `x`.
-        
-    Args:
-        x: image, torch.Variable of torch.Tensor
-        beta: See https://arxiv.org/abs/1412.0035 (fig. 2) to see effect of `beta` 
-    '''
-    dh = torch.pow(x[:,:,:,1:] - x[:,:,:,:-1], 2)
-    dw = torch.pow(x[:,:,1:,:] - x[:,:,:-1,:], 2)
-    a,b,c,d=x.shape
-    return reg_coeff*(torch.sum(torch.pow(dh[:, :, :-1] + dw[:, :, :, :-1], beta))/(a*b*c*d))
-
-class TVLoss(nn.Module):
-    def __init__(self, tv_loss_weight=1):
-        super(TVLoss, self).__init__()
-        self.tv_loss_weight = tv_loss_weight
-
-    def forward(self, x):
-        batch_size = x.size()[0]
-        h_x = x.size()[2]
-        w_x = x.size()[3]
-        count_h = self.tensor_size(x[:, :, 1:, :])
-        count_w = self.tensor_size(x[:, :, :, 1:])
-        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
-        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
-        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
-
-    @staticmethod
-    def tensor_size(t):
-        return t.size()[1] * t.size()[2] * t.size()[3]
-
-
-
-class CharbonnierLoss(nn.Module):
-    """Charbonnier Loss (L1)"""
-
-    def __init__(self, eps=1e-3):
-        super(CharbonnierLoss, self).__init__()
-        self.eps = eps
-
-    def forward(self, x, y):
-        diff = x - y
-        # loss = torch.sum(torch.sqrt(diff * diff + self.eps))
-        loss = torch.mean(torch.sqrt((diff * diff) + (self.eps*self.eps)))
-        return loss
-
-"""
-https://github.com/JunMa11/SegLoss/blob/master/losses_pytorch/dice_loss.py
-"""
-
 """
 T = tf.keras.backend.flatten(y_true)
     P = tf.keras.backend.flatten(y_pred)
@@ -81,69 +30,6 @@ class TverskyLoss(nn.Module):
         tversky = (TP + self.smooth) / (TP + self.alpha * FN + self.beta * FP + self.smooth)
 
         return 1-tversky
-
-def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
-    """
-    net_output must be (b, c, x, y(, z)))
-    gt must be a label map (shape (b, 1, x, y(, z)) OR shape (b, x, y(, z))) or one hot encoding (b, c, x, y(, z))
-    if mask is provided it must have shape (b, 1, x, y(, z)))
-    :param net_output:
-    :param gt:
-    :param axes:
-    :param mask: mask must be 1 for valid pixels and 0 for invalid pixels
-    :param square: if True then fp, tp and fn will be squared before summation
-    :return:
-    """
-    if axes is None:
-        axes = tuple(range(2, len(net_output.size())))
-
-    shp_x = net_output.shape
-    shp_y = gt.shape
-
-    with torch.no_grad():
-        if len(shp_x) != len(shp_y):
-            gt = gt.view((shp_y[0], 1, *shp_y[1:]))
-
-        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
-            # if this is the case then gt is probably already a one hot encoding
-            y_onehot = gt
-        else:
-            gt = gt.long()
-            y_onehot = torch.zeros(shp_x)
-            if net_output.device.type == "cuda":
-                y_onehot = y_onehot.cuda(net_output.device.index)
-            y_onehot.scatter_(1, gt, 1)
-
-    tp = net_output * y_onehot
-    fp = net_output * (1 - y_onehot)
-    fn = (1 - net_output) * y_onehot
-
-    if mask is not None:
-        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
-        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
-        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
-
-    if square:
-        tp = tp ** 2
-        fp = fp ** 2
-        fn = fn ** 2
-
-    tp = sum_tensor(tp, axes, keepdim=False)
-    fp = sum_tensor(fp, axes, keepdim=False)
-    fn = sum_tensor(fn, axes, keepdim=False)
-
-    return tp, fp, fn
-
-def sum_tensor(inp, axes, keepdim=False):
-    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/tensor_utilities.py
-    axes = np.unique(axes).astype(int)
-    if keepdim:
-        for ax in axes:
-            inp = inp.sum(int(ax), keepdim=True)
-    else:
-        for ax in sorted(axes, reverse=True):
-            inp = inp.sum(int(ax))
-    return inp
 
 class MeanIoU(nn.Module):
     def __init__(self, n_classes=13, smooth=1e-5):
@@ -184,6 +70,7 @@ class MeanIoUBinary(nn.Module):
         iou = (TP + self.smooth) / (TP + FN +  FP + self.smooth)
 
         return iou
+
 """
     T = tf.keras.backend.flatten(y_true)
     P = tf.keras.backend.flatten(y_pred)
@@ -203,125 +90,37 @@ class DiceCoeficient(nn.Module):
 
         intersection = torch.sum(T * P)
         return ((2 * intersection) + self.smooth) / (torch.sum(T + P) + self.smooth)
-    
+
+
+
+
 class DiceLoss(nn.Module):
-    def __init__(self, smooth=1e-5):
+    def __init__(self, beta=1, class_weights=None, class_indexes=None, smooth=1e-5):
+        super(DiceLoss, self).__init__()
+        self.beta = beta
+        self.class_weights = class_weights if class_weights is not None else 1
+        self.class_indexes = class_indexes
         self.smooth = smooth
 
+    def forward(self, y_pred, y_true):        
+        if self.class_indexes is not None:
+            y_true = torch.gather(y_true, 1, torch.Tensor(self.class_indexes))
+            y_pred = torch.gather(y_pred, 1, torch.Tensor(self.class_indexes))
 
-    def forward(self, y_pred, y_true):
-        T = y_true.flatten()
-        P = y_pred.flatten()
+        axes = (0, 2, 3)
 
-        intersection = torch.sum(T * P)
-        return 1 - (((2 * intersection) + self.smooth)/ (torch.sum(T + P) + self.smooth))
+        # calculate score
+        tp = torch.sum(y_true * y_pred, dim=axes)
+        fp = torch.sum(y_pred, dim=axes) - tp
+        fn = torch.sum(y_true, dim=axes) - tp
+
+        dice = ((1 + self.beta ** 2) * tp + self.smooth) \
+                / ((1 + self.beta ** 2) * tp + self.beta ** 2 * fn + fp + self.smooth)
+        
+        if self.class_weights is not None:
+            dice = dice * self.class_weights
+
+        dice = torch.mean(dice)
+
+        return 1 - dice
     
-    
-class GDiceCoeficient(nn.Module):
-    def __init__(self, apply_nonlin=None, smooth=1e-5):
-        """
-        Generalized Dice;
-        Copy from: https://github.com/wolny/pytorch-3dunet/blob/6e5a24b6438f8c631289c10638a17dea14d42051/unet3d/losses.py#L75
-        paper: https://arxiv.org/pdf/1707.03237.pdf
-        tf code: https://github.com/NifTK/NiftyNet/blob/dev/niftynet/layer/loss_segmentation.py#L279
-        """
-        super(GDiceCoeficient, self).__init__()
-
-        self.apply_nonlin = apply_nonlin
-        self.smooth = smooth
-
-    def forward(self, net_output, gt):
-        shp_x = net_output.shape # (batch size,class_num,x,y,z)
-        shp_y = gt.shape # (batch size,1,x,y,z)
-        # one hot code for gt
-        with torch.no_grad():
-            if len(shp_x) != len(shp_y):
-                gt = gt.view((shp_y[0], 1, *shp_y[1:]))
-
-            if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
-                # if this is the case then gt is probably already a one hot encoding
-                y_onehot = gt
-            else:
-                gt = gt.long()
-                y_onehot = torch.zeros(shp_x)
-                if net_output.device.type == "cuda":
-                    y_onehot = y_onehot.cuda(net_output.device.index)
-                y_onehot.scatter_(1, gt, 1)
-
-
-        if self.apply_nonlin is not None:
-            net_output = self.apply_nonlin(net_output)
-
-        input = flatten(net_output)
-        target = flatten(y_onehot)
-        target = target.float()
-        target_sum = target.sum(-1)
-        class_weights = Variable(1. / (target_sum * target_sum).clamp(min=self.smooth), requires_grad=False)
-
-        intersect = (input * target).sum(-1) * class_weights
-        intersect = intersect.sum()
-
-        denominator = ((input + target).sum(-1) * class_weights).sum()
-
-        return  2. * intersect / denominator.clamp(min=self.smooth)
-    
-class GeneralizedDiceLoss(nn.Module):
-    def __init__(self, apply_nonlin=None, smooth=1e-5):
-        """
-        Generalized Dice;
-        Copy from: https://github.com/wolny/pytorch-3dunet/blob/6e5a24b6438f8c631289c10638a17dea14d42051/unet3d/losses.py#L75
-        paper: https://arxiv.org/pdf/1707.03237.pdf
-        tf code: https://github.com/NifTK/NiftyNet/blob/dev/niftynet/layer/loss_segmentation.py#L279
-        """
-        super(GeneralizedDiceLoss, self).__init__()
-
-        self.apply_nonlin = apply_nonlin
-        self.smooth = smooth
-
-    def forward(self, net_output, gt):
-        shp_x = net_output.shape # (batch size,class_num,x,y,z)
-        shp_y = gt.shape # (batch size,1,x,y,z)
-        # one hot code for gt
-        with torch.no_grad():
-            if len(shp_x) != len(shp_y):
-                gt = gt.view((shp_y[0], 1, *shp_y[1:]))
-
-            if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
-                # if this is the case then gt is probably already a one hot encoding
-                y_onehot = gt
-            else:
-                gt = gt.long()
-                y_onehot = torch.zeros(shp_x)
-                if net_output.device.type == "cuda":
-                    y_onehot = y_onehot.cuda(net_output.device.index)
-                y_onehot.scatter_(1, gt, 1)
-
-
-        if self.apply_nonlin is not None:
-            net_output = self.apply_nonlin(net_output)
-
-        input = flatten(net_output)
-        target = flatten(y_onehot)
-        target = target.float()
-        target_sum = target.sum(-1)
-        class_weights = Variable(1. / (target_sum * target_sum).clamp(min=self.smooth), requires_grad=False)
-
-        intersect = (input * target).sum(-1) * class_weights
-        intersect = intersect.sum()
-
-        denominator = ((input + target).sum(-1) * class_weights).sum()
-
-        return  1-2. * intersect / denominator.clamp(min=self.smooth)
-    
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order).contiguous()
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.view(C, -1)
